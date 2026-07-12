3fc043e8387f53c0a1942c4c6b5d9ad1942a0a11
regatpeter <regatpeter@hotmail.fr>
2026-07-12 23:33:51 +0200

    Corrige le rendu du hero

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 
         e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute("href"));
+        const href = this.getAttribute("href");
+
+        if (href === "#") {
+
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+
+            return;
+
+        }
+
+        const target = document.querySelector(href);
 
         if (target) {
 
